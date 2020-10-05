08c9e49222ad9a3760b5790977e43ff401066e3a
Camilo <camilovietnam@gmail.com>
2020-10-05 14:10:37 +0700

    Change default prompt

--- main.sh
+++ main.sh
@@ -1,8 +1,7 @@
 ########################################
 #####       This is the main       #####
-##### 		Shell script           #####
-##### 		                       #####
-#####		v1.0 2020/10		   #####
+#####       Shell script           #####
+#####	    v1.0 2020/10           #####
 ########################################
 
 ### Change this to the location of your config files
@@ -25,3 +24,11 @@ if [ -f $LONG_PATH/methods.sh ]
 then
 	. $LONG_PATH/methods.sh
 fi
+
+### Change prompt
+host_prompt="\[\e[0;93m\]★\[\e[m\]"
+user_prompt="\[\e[0;93m\]\u\[\e[m\]"
+dir_prompt="\[\e[0;92m\]\w\[\e[m\]"
+git_prompt=" \[\e[0;91m\]$(parse_git_branch)\[\e[m\]"
+
+PS1="$host_prompt$user_prompt [$dir_prompt]$git_prompt $>"
